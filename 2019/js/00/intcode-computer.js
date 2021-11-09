6098745b61b5beb437e1325721be8fe5de6293ff
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2021-11-09 18:47:04 +0100

    Add some doc

--- 2019/js/00/intcode-computer.js
+++ 2019/js/00/intcode-computer.js
@@ -25,7 +25,17 @@
 const {leftPad} = require('./index');
 
 const POSITION_MODE = '0';
+
+const OP_CODE_ADDS = '01';
+const OP_CODE_MULTIPLIES = '02';
+const OP_CODE_INPUT = '03';
+const OP_CODE_OUTPUT = '04';
+const OP_CODE_JUMP_IF_TRUE = '05';
+const OP_CODE_JUMP_IF_FALSE = '06';
+const OP_CODE_LESS_THAN = '07';
+const OP_CODE_EQUALS = '08';
 const OP_CODE_STOP = '99';
+
 const OP_CODE_SIZE = 2;
 
 class IntCodeInstruction {
@@ -50,6 +60,11 @@ class IntCodeComputer {
     this._inputs = inputs.slice();
   }
 
+  /**
+   * Run cycles until opcode 99 is executed (meaning that the run is completed).
+   *
+   * @returns {number | null} The last output.
+   */
   run() {
     while (!this.halted) {
       this.runCycle();
@@ -58,6 +73,13 @@ class IntCodeComputer {
     return this.output;
   }
 
+  /**
+   * Run cycle: execute instructions until an output value is emitted or opcode 99
+   * is executed.
+   *
+   * @param {Array<number>} inputs Add new inputs.
+   * @returns {number | null} Last emitted output.
+   */
   runCycle(inputs = []) {
     if (this.halted) {
       throw new Error('Cannot run cycle, current computer is halted');
@@ -68,19 +90,46 @@ class IntCodeComputer {
     return this.output;
   }
 
+  /**
+   * Read next parameter and move instruction pointer to the next position.
+   *
+   * @param {string} mode Parameter mode (0 for position mode, 1 for immediate mode).
+   * @returns {number} The parameter value.
+   */
   nextParameter(mode) {
     const value = this._readNext();
     return mode === POSITION_MODE ? this._at(value) : value;
   }
 
+  /**
+   * Write given value at the position indicated by the instruction pointer and
+   * move the pointer to the next position.
+   *
+   * @param {number} value Value to write.
+   * @returns {void}
+   */
   write(value) {
     this._writeAt(this._readNext(), value);
   }
 
+  /**
+   * Emit output value.
+   *
+   * @param {number} value Output value.
+   * @returns {void}
+   */
   out(value) {
     this.output = value;
   }
 
+  /**
+   * Write next input at the position indicated by the instruction pointer and:
+   *
+   * - Move the instruction pointer to the next position.
+   * - Remove the input value that has been used.
+   *
+   * @returns {void}
+   */
   writeInput() {
     if (this._inputs.length === 0) {
       throw new Error('Cannot read empty input');
@@ -89,14 +138,34 @@ class IntCodeComputer {
     this.write(this._inputs.shift());
   }
 
+  /**
+   * Move instruction pointer at given position.
+   *
+   * @param {number} position New position.
+   * @returns {void}
+   */
   moveAt(position) {
     this._position = position;
   }
 
+  /**
+   * Add new input values.
+   *
+   * @param {Array<number>} inputs Input values.
+   * @returns {void}
+   * @private
+   */
   _addInputs(inputs) {
     this._inputs.push(...inputs);
   }
 
+  /**
+   * Run until the opcode 99 is found or an output value
+   * is emitted.
+   *
+   * @returns {void}
+   * @private
+   */
   _run() {
     let instruction = this._nextInstruction();
 
@@ -124,21 +193,39 @@ class IntCodeComputer {
     }
   }
 
+  /**
+   * Read instruction at the current position and move instruction pointer to the
+   * next position.
+   *
+   * @returns {IntCodeInstruction} The instruction.
+   * @private
+   */
   _nextInstruction() {
     const value = this._readNext();
     return new IntCodeInstruction(value);
   }
 
+  /**
+   * Read memory value at given position.
+   *
+   * @param {number} position The position to read.
+   * @returns {number} Value stored in memory at given position.
+   * @private
+   */
   _at(position) {
     if (position < 0 || position >= this.memory.length) {
-      throw new Error(
-          `Cannot read position: ${position}`,
-      );
+      throw new Error(`Cannot read position: ${position}`);
     }
 
     return this.memory[position];
   }
 
+  /**
+   * Read memory value and move instruction pointer to the next position.
+   *
+   * @returns {number} Value stored in memory at current position.
+   * @private
+   */
   _readNext() {
     // Read current memory entry, and move on to the next position.
     return this._at(this._position++);
@@ -149,9 +236,8 @@ class IntCodeComputer {
   }
 }
 
-
 const opcodes = {
-  '01': {
+  [OP_CODE_ADDS]: {
     execute({computer, instruction}) {
       const x = computer.nextParameter(
           instruction.parameterMode(0),
@@ -165,7 +251,7 @@ const opcodes = {
     },
   },
 
-  '02': {
+  [OP_CODE_MULTIPLIES]: {
     execute({computer, instruction}) {
       const x = computer.nextParameter(
           instruction.parameterMode(0),
@@ -179,13 +265,13 @@ const opcodes = {
     },
   },
 
-  '03': {
+  [OP_CODE_INPUT]: {
     execute({computer}) {
       computer.writeInput();
     },
   },
 
-  '04': {
+  [OP_CODE_OUTPUT]: {
     execute({computer, instruction}) {
       const value = computer.nextParameter(
           instruction.parameterMode(0),
@@ -195,7 +281,7 @@ const opcodes = {
     },
   },
 
-  '05': {
+  [OP_CODE_JUMP_IF_TRUE]: {
     execute({computer, instruction}) {
       const x = computer.nextParameter(
           instruction.parameterMode(0),
@@ -211,7 +297,7 @@ const opcodes = {
     },
   },
 
-  '06': {
+  [OP_CODE_JUMP_IF_FALSE]: {
     execute({computer, instruction}) {
       const x = computer.nextParameter(
           instruction.parameterMode(0),
@@ -227,7 +313,7 @@ const opcodes = {
     },
   },
 
-  '07': {
+  [OP_CODE_LESS_THAN]: {
     execute({computer, instruction}) {
       const x = computer.nextParameter(
           instruction.parameterMode(0),
@@ -241,7 +327,7 @@ const opcodes = {
     },
   },
 
-  '08': {
+  [OP_CODE_EQUALS]: {
     execute({computer, instruction}) {
       const x = computer.nextParameter(
           instruction.parameterMode(0),
@@ -256,10 +342,10 @@ const opcodes = {
   },
 };
 
-function intcode(initialMemory, inputs = []) {
+function intcode(initialMemory, initialInputs = []) {
   const computer = new IntCodeComputer({
-    memory: initialMemory.slice(),
-    inputs,
+    memory: initialMemory,
+    inputs: initialInputs,
   });
 
   const output = computer.run();
