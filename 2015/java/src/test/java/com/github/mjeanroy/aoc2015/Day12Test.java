1bbc4eb66b69b5eb0a5fc9f0070f8d70793afa7f
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2021-11-25 23:47:59 +0100

    Add 2015 - day 12 - part 01

---
+++ 2015/java/src/test/java/com/github/mjeanroy/aoc2015/Day12Test.java
@@ -0,0 +1,53 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2021 Mickael Jeanroy
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.github.mjeanroy.aoc2015;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class Day12Test {
+
+	@Nested
+	class Part01 {
+		@Test
+		void it_should_compute_sample() {
+			assertThat(Day12.part01("sample1.txt")).isEqualTo(6L);
+			assertThat(Day12.part01("sample2.txt")).isEqualTo(6L);
+			assertThat(Day12.part01("sample3.txt")).isEqualTo(3L);
+			assertThat(Day12.part01("sample4.txt")).isEqualTo(3L);
+			assertThat(Day12.part01("sample5.txt")).isEqualTo(0L);
+			assertThat(Day12.part01("sample6.txt")).isEqualTo(0L);
+			assertThat(Day12.part01("sample7.txt")).isEqualTo(0L);
+			assertThat(Day12.part01("sample8.txt")).isEqualTo(0L);
+		}
+
+		@Test
+		void it_should_compute_input() {
+			assertThat(Day12.part01("input.txt")).isEqualTo(119433L);
+		}
+	}
+}
